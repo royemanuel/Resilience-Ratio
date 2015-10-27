6fae8013332bcae44f4f05bc86088bcf51767f29
Roy N Emanuel II <roy.emanuel@gmail.com>
2015-10-26 21:07:25 -0400

    Still working it.

--- server.R
+++ server.R
@@ -4,32 +4,34 @@ library(ggplot2)
 # This is a calculation for two different resilience profiles over
 # a performance threshold from 0 -> 1
 
-resParams <- function(init, rec, horz, ti, tr, th, thresh){
-    c((init * ti) / (thresh*ti), (rec * tr) / (thresh*tr),
-            (horz * th) / (thresh*th))
+# this will be a very simple model, where initial performance and
+# post recovery performance are always above the margin.
+resParams <- function(rec, ti, tr, th, thresh){
+    if (rec < thresh){
+        rVal <- rec
+    } else {
+        rVal <- thresh
+    }
+    rVal / thresh
 }
 # Find the resilience of a single profile
-res <- function(init, rec, horz, ti, tr, th, thresh){
-    rp <- resParams(init, rec, horz, ti, tr, th, thresh)
-    (rp[1]*ti + rp[2]*tr + rp[3]*th)/(ti + tr + th)
+res <- function(rec, ti, tr, th, thresh){
+    rp <- resParams(rec, ti, tr, th, thresh)
+    (ti + rp * tr + th)/(ti + tr + th)
 }
 
-resrat <- function(init1, init2, rec1, rec2,
-                horz1, horz2, ti1, ti2, tr1, tr2, th1, th2, thresh){
-    rp1 <- resParams(init1, rec1, horz1, ti1, tr1, th1, thresh)
-    rp2 <- resParams(init2, rec2, horz2, ti2, tr2, th2, thresh)
-    res1 <- (rp1[1]*ti1 + rp1[2]*tr1 + rp1[3]*th1)/(ti1 + tr1 + th1)
-    res2 <- (rp2[1]*ti2 + rp2[2]*tr2 + rp2[3]*th2)/(ti2 + tr2 + th2)
+resrat <- function(rec1, rec2,
+                ti1, ti2, tr1, tr2, th1, th2, thresh){
+    res1 <- res(rec1, ti1, tr1, th1, thresh)
+    res2 <- res(rec2, ti2, tr2, th2, thresh)
     res1/res2
 }
 
-threshold <- 1:1000/1000
+threshold <- (1:1000)/1000
 shinyServer(function(input, output){
         output$Res1 <- renderPlot({
         p <- ggplot(data.frame(x=threshold), aes(x)) +
-            stat_function(fun=function(x)res(init=input$Q.i.1,
-                              rec=input$Q.r.1,
-                              horz=input$Q.h.1,
+            stat_function(fun=function(x)res(rec=input$Q.r.1,
                               ti=input$t.i.1,
                               tr=input$t.r.1,
                               th=input$t.h.1,
@@ -40,9 +42,9 @@ shinyServer(function(input, output){
     })
     output$Res2 <- renderPlot({
         n <- ggplot(data.frame(x=threshold), aes(x)) +
-            stat_function(fun=function(x)res(init=input$Q.i.2,
+            stat_function(fun=function(x)res(#$init=input$Q.i.2,
                               rec=input$Q.r.2,
-                              horz=input$Q.h.2,
+                              #horz=input$Q.h.2,
                               ti=input$t.i.2,
                               tr=input$t.r.2,
                               th=input$t.h.2,
@@ -53,12 +55,12 @@ shinyServer(function(input, output){
     })
     output$Ratio.v.Threshold <- renderPlot({
         p <- ggplot(data.frame(x=threshold), aes(x)) +
-            stat_function(fun=function(x)resrat(init1=input$Q.i.1,
-                              init2=input$Q.i.2,
+            stat_function(fun=function(x)resrat(#init1=input$Q.i.1,
+                              #init2=input$Q.i.2,
                               rec1=input$Q.r.1,
                               rec2=input$Q.r.2,
-                              horz1=input$Q.h.1,
-                              horz2=input$Q.h.2,
+                              #horz1=input$Q.h.1,
+                              #horz2=input$Q.h.2,
                               ti1=input$t.i.1,
                               ti2=input$t.i.2,
                               tr1=input$t.r.1,
